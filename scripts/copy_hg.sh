3e90103962dbe1869574bde3b8f7e2bc522a5d31
Zolotarev P <pvzolotarev@1cbit.ru>
2026-05-22 15:13:02 +0300

    скрипт копирования Дарг-Кох

---
+++ scripts/copy_hg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r ../ha_config/custom_components/ ../../custom_components/
+cp -r ../ha_config/www/ ../../www/
+cp -r ../ha_config_dk/lovelace/ ../../lovelace/
+cp ../ha_config_dk/*.yaml ../../
