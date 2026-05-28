d1f13b07b1dbe8bcd4dcbc366db9f56247f05284
Zolotarev P <pvzolotarev@1cbit.ru>
2026-05-28 21:50:57 +0300

    скрипты копирования 5 озеро

--- scripts/copy_5ozero.sh
+++ scripts/copy_5ozero.sh
@@ -2,5 +2,5 @@
 
 cp -r ../ha_config/custom_components/ ../../custom_components/
 cp -r ../ha_config/www/ ../../www/
-cp -r ../ha_config_dk/lovelace/ ../../lovelace/
-cp ../ha_config_dk/*.yaml ../../
+cp -r ../ha_config_5ozer/lovelace/ ../../lovelace/
+cp ../ha_config_5ozer/*.yaml ../../
