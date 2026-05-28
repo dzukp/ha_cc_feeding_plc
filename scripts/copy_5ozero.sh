62861406d5322aac779f66535f44e571400e08a7
Zolotarev P <pvzolotarev@1cbit.ru>
2026-05-28 22:07:21 +0300

    скрипты копирования

--- scripts/copy_5ozero.sh
+++ scripts/copy_5ozero.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -r ../ha_config/custom_components/ ../../custom_components/
-cp -r ../ha_config/www/ ../../www/
-cp -r ../ha_config_5ozer/lovelace/ ../../lovelace/
+cp -r ../ha_config/custom_components/ ../../
+cp -r ../ha_config/www/ ../../
+cp -r ../ha_config_5ozer/lovelace/ ../../
 cp ../ha_config_5ozer/*.yaml ../../
--- scripts/copy_hg.sh
+++ scripts/copy_hg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -r ../ha_config/custom_components/ ../../custom_components/
-cp -r ../ha_config/www/ ../../www/
-cp -r ../ha_config_dk/lovelace/ ../../lovelace/
+cp -r ../ha_config/custom_components/ ../../
+cp -r ../ha_config/www/ ../../
+cp -r ../ha_config_dk/lovelace/ ../../
 cp ../ha_config_dk/*.yaml ../../
